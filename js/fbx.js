3187d6f1257fcc1bc9df54dd485f7947483bde6e
Keisuke SATO <riafweb@gmail.com>
2011-12-19 22:32:36 +0900

    wait

--- js/fbx.js
+++ js/fbx.js
@@ -6,115 +6,126 @@
  * @author Keisuke SATO <sato@crocos.co.jp>
  */
 
-(function($, undefined) {
-    $(function() {
+(function($, undefined){$(function(){
+  var tagname = function(tagname) {
+    return 'fbx\\:'+tagname;
+  };
 
-        var tagname = function(tagname) {
-            return 'fbx\\:'+tagname;
-        };
+  var arrayChunk = function(array, size) {
+    var len = array.length;
+    var ret = [];
+    var i, f = Math.ceil(len / size);
 
-        var arrayChunk = function(array, size) {
-            var len = array.length;
-            var ret = [];
-            var i, f = Math.ceil(len / size);
+    for(i=0; i<f; i++) {
+      var offset = i * size;
+      var p = array.slice(offset, offset + size);
+      ret.push(p);
+    }
 
-            for(i=0; i<f; i++) {
-                var offset = i * size;
-                var p = array.slice(offset, offset + size);
-                ret.push(p);
-            }
+    return ret;
+  };
 
-            return ret;
-        };
+  var arrayUnique = function(array) {
+    var storeArr = [];
+    var ret = [];
+    var i, len = array.length;
 
-        var arrayUnique = function(array) {
-            var storeArr = [];
-            var ret = [];
-            var i, len = array.length;
+    for (i=0; i<len; i++) {
+      if (!storeArr[array[i]]) {
+        storeArr[array[i]] = 1;
+        ret[ret.length] = array[i];
+      }
+    }
 
-            for (i=0; i<len; i++) {
-                if (!storeArr[array[i]]) {
-                    storeArr[array[i]] = 1;
-                    ret[ret.length] = array[i];
-                }
-            }
+    return ret;
+  }
 
-            return ret;
+  var replaceTag = function(tag, text) {
+    tag.after($('<span>').text(text).addClass('-fbx-'+tag.attr('field')));
+    tag.remove();
+  };
+
+  var fbx_parse = function() {
+    // fbx:graph
+    //
+    // attributes:
+    //   value
+    //   field
+    (function() {
+      var graph = $(tagname('graph'));
+      var fields = {};
+
+      graph.each(function() {
+        var tag = $(this);
+        var tagField = tag.attr('field');
+        if (!fields[tagField]) {
+          fields[tagField] = [];
         }
+        fields[tagField][fields[tagField].length] = tag.attr('value');
+      });
 
-        var replaceTag = function(tag, text) {
-            tag.after($('<span>').text(text).addClass('-fbx-'+tag.attr('field')));
-            tag.remove();
-        };
+      $.each(fields, function(field, values) {
+        var values = arrayUnique(values);
+        var chunkedValues = arrayChunk(values, 20);
+        var i, len = chunkedValues.length;
 
-        // fbx:graph
-        //
-        // attributes:
-        //   value
-        //   field
-        (function() {
-            var graph = $(tagname('graph'));
-            var fields = {};
-
-            graph.each(function() {
-                var tag = $(this);
-                var tagField = tag.attr('field');
-                if (!fields[tagField]) {
-                    fields[tagField] = [];
-                }
-                fields[tagField][fields[tagField].length] = tag.attr('value');
+        for (i=0; i<len; i++) {
+          FB.api('?fields='+field+'&ids='+chunkedValues[i].join(','), function(response) {
+            $.each(response, function(id, val) {
+              replaceTag($(tagname('graph')+'[value='+id+'][field='+field+']'), val[field]);
             });
+          });
+        }
+      });
+    })();
 
-            $.each(fields, function(field, values) {
-                var values = arrayUnique(values);
-                var chunkedValues = arrayChunk(values, 20);
-                var i, len = chunkedValues.length;
+    // fbx:profile
+    //
+    // attributes
+    //   value
+    //   field
+    (function() {
+      var tags = $(tagname('profile'));
+      var fields = {};
 
-                for (i=0; i<len; i++) {
-                    FB.api('?fields='+field+'&ids='+chunkedValues[i].join(','), function(response) {
-                        $.each(response, function(id, val) {
-                            replaceTag($(tagname('graph')+'[value='+id+'][field='+field+']'), val[field]);
-                        });
-                    });
-                }
+      tags.each(function() {
+        var tag = $(this);
+        var tagField = tag.attr('field');
+        if (!fields[tagField]) {
+          fields[tagField] = [];
+        }
+        fields[tagField][fields[tagField].length] = tag.attr('value');
+      });
+
+      $.each(fields, function(field, values) {
+        var values = arrayUnique(values);
+        var chunkedValues = arrayChunk(values, 20);
+        var i, len = chunkedValues.length;
+
+        for (i=0; i<len; i++) {
+          FB.api({
+            method: 'fql.query',
+            query: 'SELECT id,'+field+' FROM profile WHERE id IN ('+chunkedValues[i].join(',')+')'
+          }, function(response) {
+            $.each(response, function() {
+              replaceTag($(tagname('profile')+'[value='+this.id+'][field='+field+']'), this[field]);
             });
-        })();
+          });
+        }
+      });
+    })();
+  }
 
-        // fbx:profile
-        //
-        // attributes
-        //   value
-        //   field
-        (function() {
-            var tags = $(tagname('profile'));
-            var fields = {};
+  var wait = function(cb) {
+    if ('FB' in window && FB.getAccessToken()) {
+      cb();
+    } else {
+      setTimeout(function() {
+        wait(cb);
+      }, 250);
+    }
+  };
 
-            tags.each(function() {
-                var tag = $(this);
-                var tagField = tag.attr('field');
-                if (!fields[tagField]) {
-                    fields[tagField] = [];
-                }
-                fields[tagField][fields[tagField].length] = tag.attr('value');
-            });
+  wait(fbx_parse);
 
-            $.each(fields, function(field, values) {
-                var values = arrayUnique(values);
-                var chunkedValues = arrayChunk(values, 20);
-                var i, len = chunkedValues.length;
-
-                for (i=0; i<len; i++) {
-                    FB.api({
-                        method: 'fql.query',
-                        query: 'SELECT id,'+field+' FROM profile WHERE id IN ('+chunkedValues[i].join(',')+')'
-                    }, function(response) {
-                        $.each(response, function() {
-                            replaceTag($(tagname('profile')+'[value='+this.id+'][field='+field+']'), this[field]);
-                        });
-                    });
-                }
-            });
-        })();
-
-    });
-})(jQuery);
+});})(jQuery);
